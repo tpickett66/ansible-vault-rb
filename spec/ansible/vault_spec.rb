0d4292370030c56f1833ebe76267362cd0a209bf
sunakan <sunakan03@gmail.com>
2018-12-01 23:42:54 +0900

    Add ability to decrypt for encrpyted text.

--- spec/ansible/vault_spec.rb
+++ spec/ansible/vault_spec.rb
@@ -84,5 +84,19 @@ module Ansible
         expect(vault.inspect).to include 'this...'
       end
     end
+
+    describe '.decrypt(text:, password:)' do
+      let(:text) { File.read(fixture_path('empty.yml')) }
+      let(:plaintext) { "this is my sekret, there are many like it...\n" }
+      it 'must return the contents of text encrypted using ansible-vault' do
+        content = Vault.decrypt(text: text, password: 'ansible')
+        expect(content).to eq "---\n"
+      end
+
+      it 'must return the plaintext' do
+        content = Vault.decrypt(text: plaintext, password: 'ansible')
+        expect(content).to eq plaintext
+      end
+    end
   end
 end
